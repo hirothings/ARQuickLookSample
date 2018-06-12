786257d019bd1dcc025ef9566e9acf8741ab022f
hirothings <hiroshings.job1987@gmail.com>
2018-06-12 18:55:30 +0900

    Initial Commit

---
+++ ARQuickLookSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARQuickLookSample
+//
+//  Created by hirothings on 2018/06/12.
+//  Copyright © 2018年 hirothings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
